f8d7ff8551b53747fc4246cc0c86fad5b2f5b484
Wesley Bernardes <bw3sley@gmail.com>
2022-11-01 02:59:33 -0300

    :sparkles: feat: set up prisma and create first migration

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response, response } from "express";
+
+import multer from "multer";
+
+const multerConfig = multer();
+
+const router = Router();
+
+router.post("/products", multerConfig.single("file"), (request: Request, response: Response) => {
+    console.log(request.file);
+    
+    return response.send();
+})
+
+export { router }
